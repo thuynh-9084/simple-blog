499d4cd0967fbaf2787c0277e31b80bb3b91e345
Tan Huynh <179985401+thuynh-9084@users.noreply.github.com>
2024-12-31 01:06:55 -0800

    Change method parameter for the requested object

--- src/main/java/com/simpleblog/controller/PostController.java
+++ src/main/java/com/simpleblog/controller/PostController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,9 +32,9 @@ public class PostController {
     }
 
     @PostMapping("/posts")
-    public ResponseEntity<Post> createPost(String title, String content){
-        Post post = postService.createPost(title,content);
-        return new ResponseEntity<>(post,HttpStatus.OK);
+    public ResponseEntity<Post> createPost(@RequestBody Post newPost){
+        Post post = postService.createPost(new Post(newPost.getTitle(),newPost.getContent(),newPost.getLocalDate()));
+        return new ResponseEntity<>(post,HttpStatus.CREATED);
     }
 
     @GetMapping("/posts/{id}")
