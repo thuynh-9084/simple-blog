50246393050138ac7762137317b5b38ff23fc16c
Tan Huynh <179985401+thuynh-9084@users.noreply.github.com>
2024-12-30 21:40:17 -0800

    Remove login view controller

--- src/main/java/com/simpleblog/config/WebConfig.java
+++ src/main/java/com/simpleblog/config/WebConfig.java
@@ -10,6 +10,5 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/login").setViewName("login");
     }
 }
