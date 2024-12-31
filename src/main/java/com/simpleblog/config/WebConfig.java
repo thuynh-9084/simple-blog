eb57e7ba8c3c2f9f589f1acfa6f0323bbe5187d9
Tan Huynh <179985401+thuynh-9084@users.noreply.github.com>
2024-12-31 01:04:41 -0800

    Added view controller for post

--- src/main/java/com/simpleblog/config/WebConfig.java
+++ src/main/java/com/simpleblog/config/WebConfig.java
@@ -10,5 +10,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
+        registry.addViewController("/post").setViewName("post");
     }
 }
