342be60cf891716c127d7f070cd869160db5ef4b
Tan Huynh <179985401+thuynh-9084@users.noreply.github.com>
2025-01-05 22:23:30 -0800

    Added PUT and DELETE methods for posts

--- src/main/java/com/simpleblog/controller/PostController.java
+++ src/main/java/com/simpleblog/controller/PostController.java
@@ -8,10 +8,12 @@ import org.springframework.validation.BindingResult;
 import com.simpleblog.model.Post;
 import com.simpleblog.service.PostService;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class PostController {
@@ -40,6 +42,21 @@ public class PostController {
     public String getPostById(@PathVariable Long id, Model model) throws Exception {
         model.addAttribute("post", postService.findPostById(id));
         return "post-details";
+    }
+
+    @DeleteMapping("/post/{id}")
+    public String deletePostById(@PathVariable Long id) {
+        postService.deletePostById(id);
+        return "post";
+    }
+
+    @PutMapping("/post/{id}")
+    public String updatePost(@PathVariable Long id, @ModelAttribute Post updatedPost, Model model) throws Exception {
+        Post existingPost = postService.findPostById(id);
+        existingPost.setTitle(updatedPost.getTitle());
+        existingPost.setContent(updatedPost.getContent());
+        model.addAttribute("post", postService.createPost(existingPost));
+        return "post";
     }
 
 }
