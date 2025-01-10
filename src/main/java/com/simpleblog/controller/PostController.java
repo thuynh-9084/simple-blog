d2179d932d4c929c5795750d38319ae3eed3a422
Tan Huynh <179985401+thuynh-9084@users.noreply.github.com>
2025-01-10 00:56:55 -0800

    Removed PutMapping and DeleteMapping while also added view controller for post edit

--- src/main/java/com/simpleblog/controller/PostController.java
+++ src/main/java/com/simpleblog/controller/PostController.java
@@ -1,5 +1,7 @@
 package com.simpleblog.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -8,12 +10,10 @@ import org.springframework.validation.BindingResult;
 import com.simpleblog.model.Post;
 import com.simpleblog.service.PostService;
 
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 
 @Controller
 public class PostController {
@@ -24,7 +24,7 @@ public class PostController {
     @GetMapping("/post")
     public String getAllPost(Model model) {
         model.addAttribute("posts", postService.findAllPosts());
-        model.addAttribute("post", new Post());
+        model.addAttribute("post", new Post("","",LocalDate.now()));
         return "post";
     }
 
@@ -34,8 +34,9 @@ public class PostController {
             model.addAttribute("post", postService.findAllPosts());
             return "post";
         }
+        post.setLocalDate(LocalDate.now());
         postService.createPost(post);
-        return "post";
+        return "redirect:/post";
     }
 
     @GetMapping("/post/{id}")
@@ -44,19 +45,22 @@ public class PostController {
         return "post-details";
     }
 
-    @DeleteMapping("/post/{id}")
+    @PostMapping("/post/{id}")
     public String deletePostById(@PathVariable Long id) {
         postService.deletePostById(id);
-        return "post";
+        return "redirect:/post";
     }
 
-    @PutMapping("/post/{id}")
+    @PostMapping("/post/edit/{id}")
     public String updatePost(@PathVariable Long id, @ModelAttribute Post updatedPost, Model model) throws Exception {
-        Post existingPost = postService.findPostById(id);
-        existingPost.setTitle(updatedPost.getTitle());
-        existingPost.setContent(updatedPost.getContent());
-        model.addAttribute("post", postService.createPost(existingPost));
-        return "post";
+        postService.updatePost(id, updatedPost);
+        return "redirect:/post";
+    }
+
+    @GetMapping("/post/edit/{id}")
+    public String getEditFormById(@PathVariable Long id, Model model) throws Exception {
+        model.addAttribute("post", postService.findPostById(id));
+        return "post-edit";
     }
 
 }
